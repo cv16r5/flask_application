6b7d873866260e677f77107132f0fb02ef5012cc
cv16r5 <przeeemo07@wp.pl>
2019-03-26 22:23:24 +0100

    auppdate

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def requires_user_session(func):
 @app.route('/hello')
 @requires_user_session
 def hello():
-    return render_template('greeting.html', user =session['username'])
+    return render_template('greeting.html', name =session['username'])
 
 @app.route('/logout', methods=['GET', 'POST'])
 @requires_user_session
