095f945378a14530f0da84452f81e3f1d8fade99
cv16r5 <przeeemo07@wp.pl>
2019-04-01 22:50:00 +0200

    deploy sqlite

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ Created on Mon Apr  1 20:12:29 2019
 import sqlite3
 from flask import (Flask,
                    g,
-                   jsonify
+                   jsonify,
+                   request
                    )
-
+#import os
 app = Flask(__name__)
 
-#DATABASE = '/home/admin5/python_project/level-up-python/sqlite_source/chinook.db'
+#os.chdir(r'/home/admin5/python_project/level-up-python/')
+
 DATABASE='chinook.db'
 def get_db():
     db = getattr(g, '_database', None)
@@ -22,12 +24,23 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
-@app.route('/tracks', methods = ['GET','POST'])
+@app.route('/tracks', methods = ['GET'])
 def name_track():
+    id_artist = request.args.get('artist', None)
     db = get_db()
-    data = db.execute(
-        'SELECT Name FROM tracks ORDER BY Name ASC').fetchall()
+    if id_artist:
+        data = db.execute(
+                '''
+                SELECT tracks.name FROM tracks
+                JOIN albums ON tracks.albumid = albums.albumid
+                JOIN artists ON albums.artistid = artists.artistid
+                where artists.name =?
+                order by tracks.name asc;''',(id_artist,)).fetchall()
+    else:
+        data = db.execute(
+                'SELECT Name FROM tracks ORDER BY Name ASC').fetchall()
     return jsonify(data)
+
 
 
 @app.teardown_appcontext
