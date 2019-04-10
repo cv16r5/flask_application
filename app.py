083c7df97da9046ef48110c06624ee2334aecfb3
cv16r5 <przeeemo07@wp.pl>
2019-04-10 19:04:13 +0200

    datebase

--- app.py
+++ app.py
@@ -1,49 +1,35 @@
+import os
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr  1 20:12:29 2019
-
-@author: admin5
-"""
-
-
-from flask import (Flask,
-                   g,
-                   jsonify,
-                   request
-                   )
-
+os.chdir(r'/home/admin5/python_project/level-up-python/')
+from flask import Flask, abort, render_template, request,jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from wtforms import (Form,
                      StringField, 
                      validators,
                      IntegerField
                      )
+import models
+from models import Base
 
-from functools import wraps
-import sqlite3
 
-#import os
+# DATABASE_URL = os.environ['DATABASE_URL']
+
+engine = create_engine("postgresql://postgres:test1234@localhost:5432/chinook")
+# engine = create_engine(DATABASE_URL)
+
+db_session = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+)
+
+Base.query = db_session.query_property()
+
 app = Flask(__name__)
 
-#os.chdir(r'/home/admin5/python_project/level-up-python/')
-
-DATABASE='chinook.db'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-        db.execute('PRAGMA foreign_keys = ON')
-        db.row_factory = sqlite3.Row
-    return db
 
 @app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
+def shutdown_session(exception=None):
+    db_session.remove()
 
 
 class InvalidUsage(Exception):
@@ -67,146 +53,104 @@ def handle_invalid_usage(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
+#@app.route("/longest_tracks", methods=["GET", "PATCH"])
+#def artists():
+#    if request.method == "GET":
+#        return get_artists()
+#    elif request.method == "PATCH":
+#        return patch_artist()
+#    abort(405)
 
 
 
-
-
-@app.route('/tracks', methods = ['GET','POST'])
-def tracks():
-    if request.method=='GET':
-        return get_tracks()
-    else:
-        return post_new_record()
-        
+@app.route("/longest_tracks")
+def longest_tracks():
+    try:
+        tracks = db_session.query(models.Track).order_by(models.Track.milliseconds.desc()).limit(10).all() 
+        result=[]
+        for j,row in enumerate(tracks):
+            list_result=({c.name: str(getattr(row,c.name)) for c in row.__table__.columns})          
+            result.append(dict(list_result))
+        return jsonify(result)
+    except Exception as e:
+        return e
 
 class TracksRegistrationForm(Form):
-    artist=StringField(validators=[validators.optional()])
-    per_page=IntegerField(validators=[validators.optional(),validators.number_range(min=1)])
-    page=IntegerField(validators=[validators.optional(),validators.number_range(min=1)])
-    
-def to_json(func):
-    @wraps(func)
-    def wrapper(*args,**kwargs):
-        if func:
-            result=jsonify([row[0] for row in func().fetchall()])
-            return result
-        return func(*args,**kwargs)
-    return wrapper
-
-@to_json
-def get_tracks():
-    db = get_db()
-    
+    artist=StringField(validators=[validators.optional()])    
+@app.route("/longest_tracks_by_artist")
+def get_artist():
     form=TracksRegistrationForm(request.args)
     
-#    additional
     if not form.validate():
         return jsonify(error=form.error)
     
-    per_page=form.data['per_page'] or -1
-    limit=per_page
+    if  form.data['artist'] is None:
+        return str(404)
     
-    page=form.data['page'] or 0
-    page_index=page-1
-    offset=page_index*per_page
-    if page==0 and per_page==-1:
-        offset=-1
+    name_artist=form.data['artist']
     
-    if form.data['artist']:
-        
-        data = db.execute(
-                '''
-                SELECT tracks.name FROM tracks
-                JOIN albums ON tracks.albumid = albums.albumid
-                JOIN artists ON albums.artistid = artists.artistid
-                where artists.name =?
-                order by tracks.name COLLATE NOCASE LIMIT ? OFFSET ?;''',
-                (form.data['artist'],limit,offset))
-    else:
-        data = db.execute(
-                '''SELECT Name FROM tracks ORDER BY Name 
-                COLLATE NOCASE LIMIT ? OFFSET ?''',
-                (limit,offset))
-    
-    return data
-
-def post_new_record():
-    db = get_db()
-
-    new_track = request.get_json()
-    a1=new_track.get('Name')
-    a2=new_track.get('album_id')
-    a3=new_track.get('media_type_id')
-    a4=new_track.get('genre_id')
-    a5=new_track.get('composer')
-    a6=new_track.get('milliseconds')
-    a7=new_track.get('bytes')
-    a8=new_track.get('price')
-#   
-#    return jsonify(new_track)
-#    column_name = db.execute(
-#                'PRAGMA table_info(tracks);' ).fetchall()
-#    for clname in column_name:
-#        if new_track.get(clname[1]) and clname[1]!='TrackId'is None:
-#             raise InvalidUsage('missing {} in request data').format(clname[0])
- 
     try:
-
-        db.execute(
-            'INSERT INTO tracks '
-            '( name, AlbumId, MediaTypeId, '
-           ' GenreId, Composer, Milliseconds, Bytes, UnitPrice) '
-           ' VALUES ("{}", "{}","{}", "{}","{}", "{}","{}", "{}");'
-            .format(a1, a2,a3,a4,a5,a6,a7,a8)
-        )
-        db.commit()
-        
-    
-    except sqlite3.IntegrityError as error:
-
-        db.rollback()
-        error_reason = error.args[0]
-
-        if error_reason.startswith('UNIQUE constraint failed'):
-            raise InvalidUsage('Track already exists in ')
-
-        elif error_reason.startswith('FOREIGN KEY constraint failed'):
-            raise InvalidUsage('No country with album_id')
-
+        tracks = db_session.query(models.Track).filter(models.Track.composer==name_artist).order_by(models.Track.milliseconds.desc()).limit(10).all() 
+        result=[]
+        for j,row in enumerate(tracks):
+            list_result=({c.name: str(getattr(row,c.name)) for c in row.__table__.columns})          
+            result.append(dict(list_result))
+        if len (result)<1:
+            abort(404)
+            return str(404)
         else:
-            raise error
-
-    db_track = db.execute(
-        'SELECT * FROM tracks '
-        'where tracks.Name="{}" and '
-        'tracks.AlbumId="{}" and  tracks.MediaTypeId="{}" and '
-            'tracks.GenreId="{}" and tracks.Composer= "{}" and '
-            'tracks.Milliseconds= "{}" and tracks.Bytes= "{}" '
-            ' and tracks.UnitPrice= "{}";'.format(a1, a2,a3,a4,a5,a6,a7,a8)
-        
-    ).fetchone()
-#    return 'adsfa'
-    return jsonify(dict(db_track))
+            return jsonify(result)
+    except Exception as e:
+        return 405
+    
+#@app.route("/artists", methods=["GET", "PATCH"])
+#def artists():
+#    if request.method == "GET":
+#        return get_artists()
+#    elif request.method == "PATCH":
+#        return patch_artist()
+#    abort(405)
 
 
-@app.route('/genres', methods = ['GET'])
-def genres():
-    db = get_db()
-    data = db.execute('''
-                SELECT genres.name, count(tracks.Name) FROM tracks
-                JOIN genres ON tracks.GenreId = genres.GenreId
-                GROUP BY genres.name order by genres.name COLLATE NOCASE;'''
-                ).fetchall()
-    return jsonify(dict(data))
-    
- 
-    
+#def get_artists():
+#    artists = db_session.query(models.Artist).order_by(models.Artist.name)
+#    return "<br>".join(
+#        f"{idx}. {artist.name}" for idx, artist in enumerate(artists)
+#    )
 
 
-#if __name__ == '__main__':
-#    app.run(debug=True, use_debugger=False)    
-    
-    
-    
-    
+## Aaron Goldberg , 202
+#def patch_artist():
+#    data = request.json
+#    artist_id = data.get("artist_id")
+#    new_name = data.get("name")
+#    if artist_id is None:
+#        abort(404)
+#    artist = (
+#        db_session.query(models.Artist)
+#        .filter(models.Artist.artist_id == artist_id)
+#        .with_for_update()
+#        .one()
+#    )
+#    artist.name = new_name
+#    db_session.add(artist)
+#    db_session.commit()
+#    return "OK"
+
+#
+#@app.route("/albums")
+#def get_albums():
+#    albums = db_session.query(models.Album).order_by(models.Album.title)
+#    return render_template("albums.html", albums=albums)
+
+
+#@app.route("/playlists")
+#def get_playlists():
+#    playlists = db_session.query(models.Playlist).order_by(
+#        models.Playlist.name
+#    )
+#    return render_template("playlists.html", playlists=playlists)
+
+
+#if __name__ == "__main__":
+#    app.run(debug=False)
